3b87e821fd2234483da456f7812f415524b4d6b1
Masatoshi Iwasaki <mstshiwasaki@gmail.com>
2019-09-09 21:45:07 +0900

    Pass ChatChannel spec

--- spec/models/chat_channel_spec.rb
+++ spec/models/chat_channel_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 RSpec.describe ChatChannel, type: :model do
-  let(:chat_channel) { create(:chat_channel) }
-  let(:message) { create(:chat_channel, message_id: chat_channel.id) }
+  fixtures :users, :chat_channels
+
+  let(:chat_channel) { chat_channels(:chat_channel) }
+  let(:message) { chat_channels(:chat_channel_1) }
 
   it { is_expected.to have_many(:messages) }
   it { is_expected.to validate_presence_of(:channel_type) }
@@ -14,19 +16,19 @@ RSpec.describe ChatChannel, type: :model do
   end
 
   it "creates channel with users" do
-    chat_channel = ChatChannel.create_with_users([create(:user), create(:user)])
+    chat_channel = ChatChannel.create_with_users([users(:user_1), users(:user_2)])
     expect(chat_channel.users.size).to eq(2)
-    expect(chat_channel.has_member?(User.first)).to eq(true)
+    expect(chat_channel.has_member?(users(:user_1))).to eq(true)
   end
 
   it "lists active memberships" do
-    chat_channel = ChatChannel.create_with_users([create(:user), create(:user)])
+    chat_channel = ChatChannel.create_with_users([users(:user_1), users(:user_2)])
     expect(chat_channel.active_users.size).to eq(2)
     expect(chat_channel.channel_users.size).to eq(2)
   end
 
   it "decreases active users if one leaves" do
-    chat_channel = ChatChannel.create_with_users([create(:user), create(:user)])
+    chat_channel = ChatChannel.create_with_users([users(:user_1), users(:user_2)])
     ChatChannelMembership.last.update(status: "left_channel")
     expect(chat_channel.active_users.size).to eq(1)
     expect(chat_channel.channel_users.size).to eq(1)
